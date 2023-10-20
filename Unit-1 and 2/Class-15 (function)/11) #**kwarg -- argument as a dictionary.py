a1ec039393590a8d2bac8008742be270ad51e76f
Rithvik Matta <rithvikmatta@penguin>
2023-10-20 14:04:16 +0530

    oct 20

---
+++ Unit-1 and 2/Class-15 (function)/11) #**kwarg -- argument as a dictionary.py
@@ -0,0 +1,17 @@
+#**kwarg -- argument as a dictionary
+def f1(**kwarg):
+    print(kwarg,type(kwarg))
+    for i in kwarg.items():
+        print(i)
+    for i in kwarg.keys():
+        print(i)
+    for i in kwarg.values():
+        print(i)
+f1(a=2,b=5,c=7)
+
+def f2(x,y,*arg,**kwarg):
+    print(x,y)
+    print(arg)
+    print(kwarg)
+f2(6,7,8,9,3,a=34,b=23,c=56)
+
