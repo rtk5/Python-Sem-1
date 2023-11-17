fe873167cd210b4f2405e5813fcb2469e3f9b4a1
Rithvik Matta <rithvikmatta@penguin>
2023-11-17 13:56:28 +0530

    nov 17

--- Unit-1 and 2/Class-8 (list)/# min, max, sum.py
+++ Unit-1 and 2/Class-8 (list)/# min, max, sum.py
@@ -1,7 +1,11 @@
 # min, max, sum
-a=[20,40,60,10,30,90,100]  
+#a=[20,40,60,10,30,90,100]  
+a=input("ENTER:")
 print(min(a))
 print(max(a))
+d=max(a)
+print(a.index(d))
+'''
 print(sum(a))
 
 fruits = ['apple','cherry','orange','banana','apple']
@@ -11,3 +15,4 @@ print(max(fruits))
 print(a+[6,7])
 
 print(a*2)
+'''
