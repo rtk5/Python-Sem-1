7ccb330e5570897878bf933864bda5cd21f0655f
Rithvik Matta <rithvikmatta@penguin>
2023-11-26 17:24:02 +0530

    nov 26

--- Unit-3 (functions)/Topic-12 (GUI)/2) #button widget.py
+++ Unit-3 (functions)/Topic-12 (GUI)/2) #button widget.py
@@ -2,7 +2,9 @@
 from tkinter import *
 win=Tk()
 win.title("Tkinter Demonstration")
-win.geometry('300x200')
+win.geometry('700x900')
 b=Button(win,text='Submit')
+c=Button(win,text='Exit')
 b.pack()
+c.pack()
 win.mainloop()
